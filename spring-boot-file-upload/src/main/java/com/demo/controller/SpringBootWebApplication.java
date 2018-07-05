d1fead0a712725f814aa29ef0acd07eb097755ab
jregalla <j.regalla@apple.com>
2018-07-04 22:53:13 -0700

    demo spring

--- spring-boot-file-upload/src/main/java/com/demo/controller/SpringBootWebApplication.java
+++ spring-boot-file-upload/src/main/java/com/demo/controller/SpringBootWebApplication.java
@@ -10,13 +10,16 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringBootWebApplication {
 
+
+
 	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 
-	// Tomcat large file upload connection reset
+	
+	
 	
 	@Bean
 	public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
